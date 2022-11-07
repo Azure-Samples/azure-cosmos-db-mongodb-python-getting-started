898f27ff56a73af84a99cb3bbd032e4f9c0c595a
vmagelo <v-gelorminom@microsoft.com>
2022-11-07 15:55:05 +0100

    Changes after isort and black.

--- 001-quickstart/run.py
+++ 001-quickstart/run.py
@@ -12,21 +12,24 @@
 # <package_dependencies>
 import os
 import sys
+from random import randint
+
 import pymongo
 from dotenv import load_dotenv
-from random import randint
+
 # </package_dependencies>
 
 
 # <client_credentials>
 load_dotenv()
-CONNECTION_STRING = os.environ.get('COSMOS_CONNECTION_STRING')
+CONNECTION_STRING = os.environ.get("COSMOS_CONNECTION_STRING")
 # </client_credentials>
 
 # <constant_values>
 DB_NAME = "adventureworks"
 COLLECTION_NAME = "products"
 # </constant_values>
+
 
 def main():
     """Connect to the API for MongoDB, create DB and collection, perform CRUD operations"""
@@ -36,10 +39,12 @@ def main():
         client = pymongo.MongoClient(CONNECTION_STRING)
         # </connect_client>
         try:
-            client.server_info() # validate connection string
-        except (pymongo.errors.OperationFailure,
-                pymongo.errors.ConnectionFailure,
-                pymongo.errors.ExecutionTimeout) as err:
+            client.server_info()  # validate connection string
+        except (
+            pymongo.errors.OperationFailure,
+            pymongo.errors.ConnectionFailure,
+            pymongo.errors.ExecutionTimeout,
+        ) as err:
             sys.exit("Can't connect:" + str(err))
     except Exception as err:
         sys.exit("Error:" + str(err))
@@ -49,8 +54,8 @@ def main():
     db = client[DB_NAME]
     if DB_NAME not in client.list_database_names():
         # Database with 400 RU throughput that can be shared across the DB's collections
-        db.command({'customAction': "CreateDatabase", 'offerThroughput': 400})
-        print("Created db '{}' with shared throughput.\n". format(DB_NAME))
+        db.command({"customAction": "CreateDatabase", "offerThroughput": 400})
+        print("Created db '{}' with shared throughput.\n".format(DB_NAME))
     else:
         print("Using database: '{}'.\n".format(DB_NAME))
     # </new_database>
@@ -60,30 +65,38 @@ def main():
     collection = db[COLLECTION_NAME]
     if COLLECTION_NAME not in db.list_collection_names():
         # Creates a unsharded collection that uses the DBs shared throughput
-        db.command({'customAction': "CreateCollection", 'collection': COLLECTION_NAME})
-        print("Created collection '{}'.\n". format(COLLECTION_NAME))
+        db.command({"customAction": "CreateCollection", "collection": COLLECTION_NAME})
+        print("Created collection '{}'.\n".format(COLLECTION_NAME))
     else:
         print("Using collection: '{}'.\n".format(COLLECTION_NAME))
     # </create_collection>
 
     # <create_index>
-    indexes = [{'key': {'_id': 1}, 'name': "_id_1"},{'key': {'name': 2}, 'name': "_id_2"}]
-    db.command({'customAction': "UpdateCollection", 'collection': COLLECTION_NAME, 'indexes': indexes})
+    indexes = [
+        {"key": {"_id": 1}, "name": "_id_1"},
+        {"key": {"name": 2}, "name": "_id_2"},
+    ]
+    db.command(
+        {
+            "customAction": "UpdateCollection",
+            "collection": COLLECTION_NAME,
+            "indexes": indexes,
+        }
+    )
     print("Indexes are: {}\n".format(sorted(collection.index_information())))
     # </create_index>
 
     # <new_doc>
     """Create new document and upsert (create or replace) to collection"""
     product = {
-        'category': 'gear-surf-surfboards',
-        'name': 'Yamba Surfboard-{}'.format(randint(50, 5000)),
-        'quantity': 1,
-        'sale': False
+        "category": "gear-surf-surfboards",
+        "name": "Yamba Surfboard-{}".format(randint(50, 5000)),
+        "quantity": 1,
+        "sale": False,
     }
     result = collection.update_one(
-        {'name': product['name']},
-        {'$set': product}, 
-        upsert=True)
+        {"name": product["name"]}, {"$set": product}, upsert=True
+    )
     print("Upserted document with _id {}\n".format(result.upserted_id))
     # </new_doc>
 
@@ -95,14 +108,13 @@ def main():
     # <query_docs>
     """Query for documents in the collection"""
     print("Products with category 'gear-surf-surfboards':\n")
-    allProductsQuery = {
-        "category": "gear-surf-surfboards"
-    } 
-    for doc in collection.find(allProductsQuery).sort('name', pymongo.ASCENDING):
+    allProductsQuery = {"category": "gear-surf-surfboards"}
+    for doc in collection.find(allProductsQuery).sort("name", pymongo.ASCENDING):
         print("Found a product with _id {}: {}\n".format(doc["_id"], doc))
     # </query_docs>
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
 
 """
