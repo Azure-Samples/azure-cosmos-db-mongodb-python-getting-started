75094b8f1632b2bdb49ab51489d1ca4b5a8abfba
vmagelo <v-gelorminom@microsoft.com>
2022-11-15 21:42:31 +0100

    Get ready for getting-started use of code snippets.

---
+++ 101-client-connection-string/run.py
@@ -0,0 +1,72 @@
+# ----------------------------------------------------------------------------------------------------------
+#  Prerequisites:
+#
+# 1. An Azure Cosmos DB API for MongoDB Account.
+# 2. PyMongo installed.
+# 3. python-dotenv installed (to load environment variables from a .env file).
+# ----------------------------------------------------------------------------------------------------------
+# Code samples are used in documentation, do not change unless synchronizing with documentation.
+# ----------------------------------------------------------------------------------------------------------
+
+# <package_dependencies>
+import os
+
+import pymongo
+from dotenv import load_dotenv
+
+# </package_dependencies>
+
+
+def main():
+    """Connect to the API for MongoDB"""
+
+    try:
+        # <client_credentials>
+        load_dotenv()
+        CONNECTION_STRING = os.environ.get("COSMOS_CONNECTION_STRING")
+        client = pymongo.MongoClient(CONNECTION_STRING)
+
+        for prop, value in vars(client.options).items():
+            print("Property: {}: Value: {} ".format(prop, value))
+        # </client_credentials>
+        try:
+            client.server_info()  # validate connection string
+        except (
+            pymongo.errors.OperationFailure,
+            pymongo.errors.ConnectionFailure,
+            pymongo.errors.ExecutionTimeout,
+        ) as err:
+            sys.exit("Can't connect:" + str(err))
+    except Exception as err:
+        sys.exit("Error:" + str(err))
+
+    # <client_disconnect>
+    client.close()
+    # </client_disconnect>
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+# <console_result>
+Property: _ClientOptions__options: Value: {'replicaSet': 'globaldb', 'retrywrites': False, 'maxIdleTimeMS': 120.0, 'appName': '@cosmosdb-mongodb@', 'document_class': <class 'dict'>, 'tz_aware': False, 'connect': True, 'tls': True}
+Property: _ClientOptions__codec_options: Value: CodecOptions(document_class=dict, tz_aware=False, uuid_representation=UuidRepresentation.UNSPECIFIED, unicode_decode_error_handler='strict', tzinfo=None, type_registry=TypeRegistry(type_codecs=[], fallback_encoder=None), datetime_conversion=DatetimeConversion.DATETIME)
+Property: _ClientOptions__direct_connection: Value: None
+Property: _ClientOptions__local_threshold_ms: Value: 15
+Property: _ClientOptions__server_selection_timeout: Value: 30
+Property: _ClientOptions__pool_options: Value: <pymongo.pool.PoolOptions object at 0x0000013AC16A9170>
+Property: _ClientOptions__read_preference: Value: Primary()
+Property: _ClientOptions__replica_set_name: Value: globaldb
+Property: _ClientOptions__write_concern: Value: WriteConcern()
+Property: _ClientOptions__read_concern: Value: ReadConcern()
+Property: _ClientOptions__connect: Value: True
+Property: _ClientOptions__heartbeat_frequency: Value: 10
+Property: _ClientOptions__retry_writes: Value: False
+Property: _ClientOptions__retry_reads: Value: True
+Property: _ClientOptions__server_selector: Value: <function any_server_selector at 0x0000013AC104F490>
+Property: _ClientOptions__auto_encryption_opts: Value: None
+Property: _ClientOptions__load_balanced: Value: None
+Property: _ClientOptions__timeout: Value: None
+# </console_result>
+"""
--- README.md
+++ README.md
@@ -1,32 +1,20 @@
-# Azure Cosmos DB API For MongoDB Python Quickstart
+# Azure Cosmos DB API For MongoDB Python Quickstart and Getting Started Guides
 
-Thi repo shows how to get started with the Azure Cosmos DB API for MongoDB API in a Python application. There are two samples.
+Thi repo shows how to get started with the Azure Cosmos DB API for MongoDB API in a Python application.
 
-* The sample in the root directory, [run.py](run.py), prompts you for connection information. This sample is written with separate functions for each operation as you might do in a production application.
+* The sample in the root directory, [run.py](run.py), prompts you for connection information. This sample is written with separate functions for each operation as you might do in a production application. Try to the sample to get started quickly.
 
-* The sample in the **001-quickstart** directory, [001-quickstart/run.py](/001-quickstart/run.py), reads an environment variable, creates an AdventureWorks collection, and creates an index. The second sample provides code snippets for the related quickstart: https://learn.microsoft.com/en-us/azure/cosmos-db/mongodb/quickstart-python and is structured for simplicity.
+* The sample folders (e.g., **001-quickstart** directory) contain runnable samples used in documentation https://learn.microsoft.com/en-us/azure/cosmos-db/mongodb/quickstart-python.
 
-## Sample 1
+## Sample prerequisites
 
 Prerequisites:
 
 * An Azure Cosmos DB API for MongoDB Account
 * Python 3.8+ installed
 * [PyMongo](https://www.mongodb.com/docs/drivers/pymongo/) installed
+* [python-dotenv](https://pypi.org/project/python-dotenv/) installed to read the connection string from an *.env* file for all samples outside the root sample.
 
 Use `python run.py` to run the code which will prompt you for a connection string.
 
-## Sample 2 - Quickstart documentation
-
-The quickstart https://learn.microsoft.com/en-us/azure/cosmos-db/mongodb/quickstart-python uses the code snippets from the *001-quickstart/run.py* file and sets connection string information from and environment variable, for example, with an *.env* file and python-dotenv.
-
-Prerequisites:
-
-* An Azure Cosmos DB API for MongoDB Account
-* Python 3.8+ installed
-* [PyMongo](https://www.mongodb.com/docs/drivers/pymongo/) installed
-* [python-dotenv](https://pypi.org/project/python-dotenv/) installed to read the connection string from an *.env* file
-
-**Step 1.** Copy the *001-quickstart/.env.sample* file to *001-quickstart/.env* and fill in the connection information.
-
-**Step 2.** From the root directory, run `python 001-quickstart/run.py`.
+Make sure you define an *.env* variable with the connection string if needed for a sample.
